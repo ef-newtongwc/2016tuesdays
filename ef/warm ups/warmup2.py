b3fcc29bbba4fff895448c8b6ec2f3c185a2af5c
Ella Fontenot <ella_fontenot@newtongwc.org>
2017-01-17 20:12:39 -0500

    ella's warmup (2)

---
+++ ef/warm ups/warmup2.py
@@ -0,0 +1,9 @@
+while True:
+    print("Type Girls Who Code")
+    bob = raw_input()
+    if bob == "Girls Who Code":
+        print("Hooray!!!")
+        break
+    else:
+        print("try again")
+
