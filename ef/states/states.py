b22ad9fa27fcc09abf2f84673969b78b7f51f7d1
Ella Fontenot <ella_fontenot@newtongwc.org>
2017-02-07 20:33:40 -0500

    hi

--- ef/states/states.py
+++ ef/states/states.py
@@ -11,8 +11,6 @@ def show_states_home():
     return app.send_static_file('states_home.html')
 
 
-app.run()
-
 states = ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado', 'Connecticut', 'Delaware',
           'Florida', 'Georgia', 'Hawaii', 'Iowa', 'Idaho', 'Illinois', 'Indiana', 'Kansas', 'Kentucky', 'Louisiana',
           'Massachusetts', 'Maryland', 'Maine', 'Michigan', 'Minnesota', 'Missouri', 'Mississippi', 'Montana',
@@ -26,6 +24,17 @@ states = ['Alaska', 'Alabama', 'Arkansas', 'Arizona', 'California', 'Colorado', 
 def show_random_state():
     random_state = random.choice(states)
     return render_template('states_vary.html', state=random_state)
+
+
+@app.route("/letter")
+def show_matching_states():
+    user_letter = request.args.get('user_letter', '')
+    matching_states = []
+    for state in states:
+        if state.lower().startswith(user_letter):
+            matching_states.append(state)
+    output_html = ', '.join(matching_states)
+    return render_template('states_vary.html', user_letter=user_letter, list_of_states=output_html)
 
 
 app.run()
